afb3600609716c70653eedf409636d0be18873f1
NSQAQ <2534335301@qq.com>
2022-04-05 11:46:03 +0800

    week2_demo

--- Week2/demo.py
+++ Week2/demo.py
@@ -1,4 +1,11 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as ply
+import matplotlib.pyplot as plt
 
+# 数据处理
+data=pd.read_csv("./ex2data1.txt",names=["score_1","score_2","y"])
+data_y0=data[data.y==0]
+data_y1=data[data.y>0]
+# 查看散点图
+plt.plot(data_y0.score_1,data_y0.score_2,"ro",data_y1.score_1,data_y1.score_2,"b*")
+plt.show()
