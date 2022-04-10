5e99ec6e5dbad78973f34245b06fa5cdde465257
NSQAQ <2534335301@qq.com>
2022-04-10 15:04:39 +0800

    updata

--- Week2/ex1.py
+++ Week2/ex1.py
@@ -2,22 +2,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# 目标函数
-def logistic(z):
+# 激活函数
+def sigmoid(z):
 	dem=1+np.exp(-z)
 	return 1/dem
 
 # cost——function
 def cost_fun(x,y,theta):
-	frist=np.multiply(y, np.log(logistic(x @ theta)))
-	second=np.multiply((1-y), np.log(1 - logistic(x @ theta)))
+	frist=np.multiply(y, np.log(sigmoid(x @ theta)))
+	second=np.multiply((1-y), np.log(1 - sigmoid(x @ theta)))
 	return -np.sum(frist+second)/len(x)
 
 #梯度下降
 def b_dg(x,y,theta,alpha):
 	theta_num=theta.copy()
 	for time in range(100000):
-		logistic_num= logistic(x @ theta_num)
+		logistic_num= sigmoid(x @ theta_num)
 		for l in range(x.shape[1]):
 			theta_num[l,0]=theta_num[l,0]-alpha*(np.multiply((logistic_num-y),x[:,l])).sum()/x.shape[0]
 	return theta_num
--- Week2/ex3.py
+++ Week2/ex3.py
@@ -2,4 +2,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data_x=pd.read_excel("./data1x.xlsx")
+def sigmoid(z):
+	dem=1+np.exp(-z)
+	return 1/dem
+def cost_fun(theta,x,y):
+	pass
+# 数据初始化
+data_x=pd.read_excel("./data1x.xlsx",dtype=float,header=0)
+data_x.insert(0,"x_0",1)
+data_y=pd.read_excel("./data1y.xlsx",dtype=float)
+x=np.matrix(data_x.iloc[:,:].values).T
+y=np.matrix(data_y.iloc[:,:].values)
+y_mat=np.matrix(np.zeros((10,5000)))
+for l in range(5000):
+	y_mat[int(y[l,0]-1),l]=1
+thate1=np.matrix(np.random.rand(40,401))
+thate2=np.matrix(np.random.rand(10,40))
+sigmoid_mu=sigmoid(thate1@x)
